0004af47c24de53c89d1bbbd34216e3e05c8f6c2
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-01-21 00:49:02 +0000

    pfp fixed TRYING TO FIX SCROLLING FUNCTION

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,15 +1,19 @@
+const myface =
+  "https://raw.githubusercontent.com/MSalim16/MSPortfolio-React/main/src/images/MSALIMPICTURE%20WORK.jpeg";
+
 function HomePage() {
   return (
     <div className="Home-Page">
       <div class="landingcoloumn">
         <h1 class="fullstack">
-          Full-Stack <br></br>Developer<span class="fullstop">.</span>
+          Full Stack <br></br>Developer<span class="fullstop">.</span>
         </h1>
         <h2 class="subheading">
-          I like to craft solid and scalable frontend products with great{" "}
+          I like to craft solid and scalable full stack products with great
           <br></br> user experiences.
         </h2>
       </div>
+      <img class="landingpfp" src={myface}></img>
     </div>
   );
 }
