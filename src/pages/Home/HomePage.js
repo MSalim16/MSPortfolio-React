3fe4e62b78ef007a68c2e1f103b3368a972668da
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-01-24 10:37:51 +0000

    fixing animations

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,29 +1,65 @@
+import HTML from "../../tech/html.png";
+import CSS from "../../tech/css.png";
+import JavaScript from "../../tech/javascript.png";
+import ReactImg from "../../tech/react.png";
+import Node from "../../tech/node.png";
+import FireBase from "../../tech/firebase.png";
+import GitHub from "../../tech/github.png";
+import Postgresql from "../../tech/postgresql.png";
+import Mongo from "../../tech/mongo.png";
+
+import { motion, useScroll } from "framer-motion";
+
+import "../Home/home.css";
+
 const myface =
   "https://raw.githubusercontent.com/MSalim16/MSPortfolio-React/main/src/images/MSALIMPICTURE%20WORK.jpeg";
 
 function HomePage() {
+  const { scrollYProgress } = useScroll();
   return (
     <div className="wrapper">
       <section className="one">
         <div className="Home-Page">
           <div class="landingcoloumn">
             <h1 class="fullstack">
-              Full Stack <br></br>Developer<span class="fullstop">.</span>
+              Full Stack <br></br>
+              Developer<span class="fullstop">.</span>
             </h1>
-            <h2 class="subheading">
+            <p class="subheading">
               I like to craft solid and scalable full stack products with great
               <br></br> user experiences.
-            </h2>
+            </p>
           </div>
           <img class="landingpfp" src={myface}></img>
         </div>
       </section>
-
       <section className="two">
-        <h1>About</h1>
+        <motion.div className="skills" style={{ scaleX: scrollYProgress }}>
+          {/* Container */}
+          <div className="skills-container">
+            <div>
+              <p className="skills-header">Skills</p>
+              <p className="py-4">
+                // These are the technologies I've worked with
+              </p>
+            </div>
+
+            <div className="skills-3">
+              <div className="htmlimg">
+                <img className="htimg" src={HTML} alt="HTML icon" />
+                <p className="ht">HTML</p>
+              </div>
+            </div>
+          </div>
+        </motion.div>
       </section>
       <section className="three">
         <h1>my Tech Stack</h1>
+      </section>
+
+      <section className="four">
+        <h1>Footer</h1>
       </section>
     </div>
   );
--- src/pages/Projects/ProjectPage.js
+++ src/pages/Projects/ProjectPage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ProjectPage = () => {
-  return <div>hi from projects</div>;
+  return (
+    <div style={{ color: "black" }}>
+      <h1> hi from projects</h1>
+    </div>
+  );
 };
 
 export default ProjectPage;
