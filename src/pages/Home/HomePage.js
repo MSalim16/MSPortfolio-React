942af966ef52973db6405db85013cc4ee2ae8449
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-01-25 16:47:47 +0000

    finished skills page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -8,6 +8,11 @@ import GitHub from "../../tech/github.png";
 import Postgresql from "../../tech/postgresql.png";
 import Mongo from "../../tech/mongo.png";
 import TypeScript from "../../tech/typescript.png";
+import JEST from "../../tech/jest.png";
+import EXPRESS from "../../tech/express.png";
+import VSCODE from "../../tech/vscode.png";
+import PHOTOSHOP from "../../tech/photoshop.png";
+
 import { useEffect } from "react";
 
 import "../Home/home.css";
@@ -43,93 +48,81 @@ function HomePage() {
 
       <section className="two">
         <div className="skills">
-          <div className="skil">
+          <div>
             <p className="skills-header">Skills</p>
             <p className="skills-p">
               These are the technologies I've worked with
             </p>
           </div>
           <div className="skills-container">
-            <div className="skill-box">
-              <div className="box">
-                <img className="htimg" src={HTML} alt="HTML icon" />
-                <p className="ht">HTML</p>
-              </div>
+            <div className="box">
+              <img className="htimg" src={HTML} alt="HTML icon" />
+              <p className="ht">HTML</p>
             </div>
 
-            <div className="skill-box">
-              <div className="box">
-                <img className="htimg" src={CSS} alt="CSS icon" />
-                <p className="ht">CSS</p>
-              </div>
+            <div className="box">
+              <img className="htimg" src={CSS} alt="CSS icon" />
+              <p className="ht">CSS</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img
-                    className="htimg"
-                    src={JAVASCRIPT}
-                    alt="JAVASCRIPT icon"
-                  />
-                  <p className="ht">JAVASCRIPT</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={JAVASCRIPT} alt="JAVASCRIPT icon" />
+              <p className="ht">JAVASCRIPT</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img className="htimg" src={REACT} alt="REACT icon" />
-                  <p className="ht">REACT</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={REACT} alt="REACT icon" />
+              <p className="ht">REACT</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img className="ggithub" src={GitHub} alt="GITHUB icon" />
-                  <p className="ht">GITHUB</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="ggithub" src={GitHub} alt="GITHUB icon" />
+              <p className="ht">GITHUB</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img className="htimg" src={Node} alt="NODE JS icon" />
-                  <p className="ht">NODE JS</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={Node} alt="NODE JS icon" />
+              <p className="ht">NODE JS</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img className="htimg" src={Mongo} alt="MONGO DB icon" />
-                  <p className="ht">MONGO DB</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={Mongo} alt="MONGO DB icon" />
+              <p className="ht">MONGO DB</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img className="htimg" src={FireBase} alt="FIREBASE icon" />
-                  <p className="ht">FIREBASE</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={FireBase} alt="FIREBASE icon" />
+              <p className="ht">FIREBASE</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img
-                    className="htimg"
-                    src={Postgresql}
-                    alt="POSTGRESQL icon"
-                  />
-                  <p className="ht">POSTGRESQL</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={Postgresql} alt="POSTGRESQL icon" />
+              <p className="ht">POSTGRESQL</p>
+            </div>
 
-              <div className="skill-box">
-                <div className="box">
-                  <img
-                    className="htimg"
-                    src={TypeScript}
-                    alt="TYPESCRIPT icon"
-                  />
-                  <p className="ht">TYPESCRIPT</p>
-                </div>
-              </div>
+            <div className="box">
+              <img className="htimg" src={TypeScript} alt="TYPESCRIPT icon" />
+              <p className="ht">TYPESCRIPT</p>
+            </div>
+            <div className="box">
+              <img className="htimg" src={JEST} alt="JEST icon" />
+              <p className="ht">JEST</p>
+            </div>
+            <div className="box">
+              <img className="htimg" src={EXPRESS} alt="EXPRESS icon" />
+              <p className="ht">EXPRESS JS</p>
+            </div>
+            <div className="box">
+              <img className="htimg" src={REACT} alt="REACT NATIVE icon" />
+              <p className="ht">REACT NATIVE</p>
+            </div>
+            <div className="box">
+              <img className="htimg" src={VSCODE} alt="VSCODE icon" />
+              <p className="ht">VS CODE</p>
+            </div>
+            <div className="box">
+              <img className="htimg" src={PHOTOSHOP} alt="PHOTOSHOP icon" />
+              <p className="ht">PHOTOSHOP</p>
             </div>
           </div>
         </div>
