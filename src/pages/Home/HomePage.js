ec49b07bd7a0c17b8370b0b518410f16dfbffd81
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-01-25 13:38:54 +0000

    2nd part of  home almost done

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,22 +1,28 @@
 import HTML from "../../tech/html.png";
 import CSS from "../../tech/css.png";
-import JavaScript from "../../tech/javascript.png";
-import ReactImg from "../../tech/react.png";
+import JAVASCRIPT from "../../tech/javascript.png";
+import REACT from "../../tech/react.png";
 import Node from "../../tech/node.png";
 import FireBase from "../../tech/firebase.png";
 import GitHub from "../../tech/github.png";
 import Postgresql from "../../tech/postgresql.png";
 import Mongo from "../../tech/mongo.png";
-
-import { motion, useScroll } from "framer-motion";
+import TypeScript from "../../tech/typescript.png";
+import { useEffect } from "react";
 
 import "../Home/home.css";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const myface =
   "https://raw.githubusercontent.com/MSalim16/MSPortfolio-React/main/src/images/MSALIMPICTURE%20WORK.jpeg";
 
 function HomePage() {
-  const { scrollYProgress } = useScroll();
+  useEffect(() => {
+    AOS.init({});
+  }, []);
+
   return (
     <div className="wrapper">
       <section className="one">
@@ -34,25 +40,99 @@ function HomePage() {
           <img class="landingpfp" src={myface}></img>
         </div>
       </section>
-      <section className="two">
-        <motion.div className="skills" style={{ scaleX: scrollYProgress }}>
-          {/* Container */}
-          <div className="skills-container">
-            <div>
-              <p className="skills-header">Skills</p>
-              <p className="py-4">
-                // These are the technologies I've worked with
-              </p>
-            </div>
 
-            <div className="skills-3">
-              <div className="htmlimg">
+      <section className="two">
+        <div className="skills">
+          <div className="skil">
+            <p className="skills-header">Skills</p>
+            <p className="skills-p">
+              These are the technologies I've worked with
+            </p>
+          </div>
+          <div className="skills-container">
+            <div className="skill-box">
+              <div className="box">
                 <img className="htimg" src={HTML} alt="HTML icon" />
                 <p className="ht">HTML</p>
               </div>
             </div>
+
+            <div className="skill-box">
+              <div className="box">
+                <img className="htimg" src={CSS} alt="CSS icon" />
+                <p className="ht">CSS</p>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img
+                    className="htimg"
+                    src={JAVASCRIPT}
+                    alt="JAVASCRIPT icon"
+                  />
+                  <p className="ht">JAVASCRIPT</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img className="htimg" src={REACT} alt="REACT icon" />
+                  <p className="ht">REACT</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img className="ggithub" src={GitHub} alt="GITHUB icon" />
+                  <p className="ht">GITHUB</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img className="htimg" src={Node} alt="NODE JS icon" />
+                  <p className="ht">NODE JS</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img className="htimg" src={Mongo} alt="MONGO DB icon" />
+                  <p className="ht">MONGO DB</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img className="htimg" src={FireBase} alt="FIREBASE icon" />
+                  <p className="ht">FIREBASE</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img
+                    className="htimg"
+                    src={Postgresql}
+                    alt="POSTGRESQL icon"
+                  />
+                  <p className="ht">POSTGRESQL</p>
+                </div>
+              </div>
+
+              <div className="skill-box">
+                <div className="box">
+                  <img
+                    className="htimg"
+                    src={TypeScript}
+                    alt="TYPESCRIPT icon"
+                  />
+                  <p className="ht">TYPESCRIPT</p>
+                </div>
+              </div>
+            </div>
           </div>
-        </motion.div>
+        </div>
       </section>
       <section className="three">
         <h1>my Tech Stack</h1>
