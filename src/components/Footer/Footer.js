303f084e734aaf5c49363392771685642be020b3
MSalim16 <msalim16@DESKTOP-DAK50HL.localdomain>
2023-01-27 10:05:30 +0000

    links fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,22 +21,24 @@ const Footer = () => {
         </div>
         <h1 className="links"> QUICK LINKS </h1>
         <div className="footer-redirects">
-          <a className="home-link" href="http://localhost:3000/">
-            <p>Home</p>
-          </a>
-          <Link to="/CV">
-            <p> My CV </p>
-          </Link>
-          <Link to="/Projects">
-            <p> My Projects </p>
-          </Link>
-          <a
-            href="https://github.com/MSalim16"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p>My GitHub</p>
-          </a>
+          <div className="bottom-links">
+            <a className="home-link" href="http://localhost:3000/">
+              <p>Home</p>
+            </a>
+            <Link to="/CV">
+              <p> My CV </p>
+            </Link>
+            <Link to="/Projects">
+              <p> My Projects </p>
+            </Link>
+            <a
+              href="https://github.com/MSalim16"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p>My GitHub</p>
+            </a>
+          </div>
         </div>
         <div className="bottom-footer"></div>
         <h1 className="createdby">Created By Mohamed Salim 2023</h1>
